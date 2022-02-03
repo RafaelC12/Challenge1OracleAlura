464a7a3234e24fe975dc1bc64f85c24e84c1977b
Rafael Chagas <takumipadfoot@gmail.com>
2022-02-03 16:31:04 -0300

    Adicionando a pasta de arquivos JavaScript

---
+++ js/criptografar.js
@@ -0,0 +1,73 @@
+var botaoCriptografar = document.querySelector("#cripto");
+var botaoDescriptografar = document.querySelector("#descripto");
+var botaoCopiar = document.querySelector("#copiar");
+var mensagemSaida = document.querySelector("#saida");
+
+//criptografar mensagem//
+botaoCriptografar.addEventListener("click", function(evento) {
+
+  evento.preventDefault();
+
+  var mensagem = document.querySelector('#mensagem').value;
+  var mensagemCodificada = criptografarMsg(mensagem);
+
+});
+
+//descriptografar mensagem//
+botaoDescriptografar.addEventListener("click", function(evento) {
+
+  evento.preventDefault();
+
+  var mensagem = document.querySelector('#mensagem').value;
+  var mensagemDescodificada = descriptografarMsg(mensagem);
+
+});
+
+function criptografarMsg(mensagem) {
+
+  var mensagemArray = mensagem.split('');
+
+  for (var i = 0; i < mensagemArray.length; i++) {
+
+    if(mensagemArray[i] == mensagemArray[i].toUpperCase() && mensagemArray[i] !== " ") {
+
+      alert("Apenas letras minúsculas!");
+      break;
+
+    } else{
+
+      var textoCodificado = mensagem;
+      textoCodificado = mensagem.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat").replace(/[^a-z ]/gi,'');
+      console.log(textoCodificado);
+      mensagemSaida.value = textoCodificado;
+
+    }
+  }
+}
+
+function descriptografarMsg(mensagem) {
+
+  var mensagemArray = mensagem.split('');
+
+  for (var i = 0; i < mensagemArray.length; i++) {
+
+    if(mensagemArray[i] == mensagemArray[i].toUpperCase() && mensagemArray[i] !== " ") {
+
+      alert("Apenas letras minúsculas, sem números ou caracteres especiais!");
+      break;
+
+    } else{
+
+      var textoDescodificado = mensagem;
+      textoDescodificado = mensagem.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u").replace(/[^a-z ]/gi,'');
+      console.log(textoDescodificado);
+      mensagemSaida.value = textoDescodificado;
+
+    }
+  }
+}
+
+botaoCopiar.addEventListener("click", () => {
+  mensagemSaida.select();
+  document.execCommand("copy");
+});
